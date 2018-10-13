dbca1bb7e3cf546d4cda85c9c8947e99b1b19268
Kryspinn <33183934+Kryspinn@users.noreply.github.com>
2018-10-13 12:10:53 +0200

    app

---
+++ Warsztaty/Warsztaty.Core/ContactModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Warsztaty.Core
+{
+    [Table("Contact")]
+    public class ContactModel
+    {
+        [Key]
+        public virtual int Id { get; set; }
+
+        [MaxLength(25)]
+        public virtual string Name { get; set; }
+
+        public virtual int Number { get; set; }
+
+        public virtual string Email { get; set; }
+
+        public virtual string Phone { get; set; }
+
+        public virtual DateTime CreatedData { get; set; }
+
+        public virtual bool IsActive { get; set; }
+    }
+}
---
+++ Warsztaty/Warsztaty.ViewModel/ContactViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Warsztaty.ViewModel
+{
+    public class ContactViewModel
+    {       
+        [MaxLength(25)]
+        public virtual string Name { get; set; }
+
+        public virtual int Number { get; set; }
+
+        public virtual string Email { get; set; }
+
+        public virtual string Phone { get; set; }
+    }
+}
